efd4023a340ea05ebc8515c5405de5c9ff4b394c
LinkunGao <gaolinkun123@gmail.com>
2022-05-23 12:44:11 +1200

    update environment function

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,11 @@
 // import Scene from "./Scene/index";
 import Renderer from "./Renderer/copperRenderer";
+import { setHDRFilePath } from "./lib/environment/index";
 
 console.log(
-  "%cMedtech Heart Plugin %cBeta:v1.2.1",
+  "%cMedtech Heart Plugin %cBeta:v1.0.1",
   "padding: 3px;color:white; background:#023047",
   "padding: 3px;color:white; background:#219EBC"
 );
 
-export { Renderer };
+export { Renderer, setHDRFilePath };
