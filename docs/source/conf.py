a7c99b6294ce8abcf054133dbd3dfe097db5b354
LinkunGao <gaolinkun123@gmail.com>
2022-06-16 19:48:01 +1200

    update

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -22,7 +22,7 @@ copyright = '2022, LinkunGao'
 author = 'LinkunGao'
 
 # The full version, including alpha/beta/rc tags
-release = 'v1.7.1'
+release = 'v1.8.0'
 
 
 # -- General configuration ---------------------------------------------------
